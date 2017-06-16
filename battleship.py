950ed15886d53f3cd1fe5de7a226a44770015ff7
Lori <mclorimer@gmail.com>
2017-06-16 19:11:55 -0400

    updated printout

--- battleship.py
+++ battleship.py
@@ -3,16 +3,28 @@ class Game:
 	def __init__(self):
 		self.board = []
 		self.rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
-		self.columns = ["  1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+		self.columns = ["   1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 	
 	def print_board(self):
 		for waves in range(10):
-			self.board.append(["~"]*10)
-		print(self.columns)
+			self.board.append(["|~|"]*10)
+		print("   ".join(self.columns))
 		row = 0
 		while row <= 8:
-			print(self.rows[row], self.board[row])
+			print(self.rows[row], " ".join(self.board[row]))
 			row += 1
+
+
+class Ship:
+
+	def __init__(self):
+		self.submarine = "SSS"
+		self.aircraft_carrier = "AAAAA"
+		self.battleship = "BBBB"
+		self.small = "QQ"
+		self.destroyer = "DDD"
+
+
 
 def main():
 	gameBoard = Game()

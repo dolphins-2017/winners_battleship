0006c90b03e52483e03e8f42c506ecf9af2197f4
Lori <mclorimer@gmail.com>
2017-06-16 18:52:07 -0400

    prints a battleship board using lists and a while loop

--- battleship.py
+++ battleship.py
@@ -1,2 +1,22 @@
-def game(self):
-  pass
+class Game:
+
+	def __init__(self):
+		self.board = []
+		self.rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+		self.columns = ["  1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+	
+	def print_board(self):
+		for waves in range(10):
+			self.board.append(["~"]*10)
+		print(self.columns)
+		row = 0
+		while row < 11:
+			print(self.rows[row], self.board[row])
+			row += 1
+
+def main():
+	gameBoard = Game()
+	gameBoard.print_board()
+
+main()
+

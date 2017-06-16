9b3341c9088b51fdb35a826bc8b993ad784c8dae
Lori <mclorimer@gmail.com>
2017-06-16 19:46:02 -0400

    Made further changes to how the board is generated.

--- battleship.py
+++ battleship.py
@@ -2,17 +2,35 @@ class Game:
 
 	def __init__(self):
 		self.board = []
-		self.rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+		self.rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 		self.columns = ["   1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 	
-	def print_board(self):
+	def genBoard(self):
 		for waves in range(10):
 			self.board.append(["|~|"]*10)
+		
 		print("   ".join(self.columns))
-		row = 0
-		while row <= 8:
-			print(self.rows[row], " ".join(self.board[row]))
-			row += 1
+		A = "A " + " ".join(self.board[0])
+		B = "B " + " ".join(self.board[1])
+		C = "C " + " ".join(self.board[2])
+		D = "D " + " ".join(self.board[3])
+		E = "E " + " ".join(self.board[4])
+		F = "F " + " ".join(self.board[5])
+		G = "G " + " ".join(self.board[6])
+		H = "H " + " ".join(self.board[7])
+		I = "I " + " ".join(self.board[8])
+		J = "J " + " ".join(self.board[9])
+		print(A)
+		print(B)
+		print(C)
+		print(D)
+		print(E)
+		print(F)
+		print(G)
+		print(H)
+		print(I)
+		print(J)
+
 
 
 class Ship:
@@ -28,7 +46,8 @@ class Ship:
 
 def main():
 	gameBoard = Game()
-	gameBoard.print_board()
+	gameBoard.genBoard()
 
+	
 main()
 

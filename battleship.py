5fb958f27edcce7675234ee31e34c661d31a26a8
Lori <mclorimer@gmail.com>
2017-06-16 22:38:18 -0400

    Added the 'insertShips' function in Game class

--- battleship.py
+++ battleship.py
@@ -2,52 +2,51 @@ class Game:
 
 	def __init__(self):
 		self.board = []
-		self.rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+		self.gen = []
 		self.columns = ["   1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 	
 	def genBoard(self):
 		for waves in range(10):
-			self.board.append(["|~|"]*10)
+			self.gen.append(["|~|"]*10)
 		
-		print("   ".join(self.columns))
-		A = "A " + " ".join(self.board[0])
-		B = "B " + " ".join(self.board[1])
-		C = "C " + " ".join(self.board[2])
-		D = "D " + " ".join(self.board[3])
-		E = "E " + " ".join(self.board[4])
-		F = "F " + " ".join(self.board[5])
-		G = "G " + " ".join(self.board[6])
-		H = "H " + " ".join(self.board[7])
-		I = "I " + " ".join(self.board[8])
-		J = "J " + " ".join(self.board[9])
-		print(A)
-		print(B)
-		print(C)
-		print(D)
-		print(E)
-		print(F)
-		print(G)
-		print(H)
-		print(I)
-		print(J)
+		top = "   ".join(self.columns)
+		A = "A " + " ".join(self.gen[0])
+		B = "B " + " ".join(self.gen[1])
+		C = "C " + " ".join(self.gen[2])
+		D = "D " + " ".join(self.gen[3])
+		E = "E " + " ".join(self.gen[4])
+		F = "F " + " ".join(self.gen[5])
+		G = "G " + " ".join(self.gen[6])
+		H = "H " + " ".join(self.gen[7])
+		I = "I " + " ".join(self.gen[8])
+		J = "J " + " ".join(self.gen[9])
+		breakdown = [top, A, B, C, D, E, F, G, H, I, J]
+		for i in breakdown:
+			self.board.append(i)
 
+	def printBoard(self):
+		for i in  self.board:
+			print(i)
 
+	def insertShips(self):
+		x = 1
+		y = 3
+		submarine = ["S", "S", "S"]
+		splitlist = list(self.board[x])
+		for i in submarine:
+			splitlist[y] = i
+			newlist = "".join(splitlist)
+			self.board[x] = newlist
+			x += 1
+		self.printBoard()
 
-class Ship:
-
-	def __init__(self):
-		self.submarine = "SSS"
-		self.aircraft_carrier = "AAAAA"
-		self.battleship = "BBBB"
-		self.small = "QQ"
-		self.destroyer = "DDD"
 
 
 
 def main():
-	gameBoard = Game()
-	gameBoard.genBoard()
-
+	# gameBoard = Game()
+	# gameBoard.genBoard()
+	# gameBoard.printBoard()
+	pass
 	
 main()
-
